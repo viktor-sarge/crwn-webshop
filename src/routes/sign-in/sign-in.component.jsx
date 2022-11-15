3b8fca9d33121849d787c2419849dadebcfc3d5a
Viktor Sarge <viktor.sarge@regionhalland.se>
2022-11-15 09:32:28 +0200

    feat(auth): ✨ add sign in with google redirect
    Lagt till sign in med redirect

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,9 +1,27 @@
+import { useEffect } from 'react';
+import { getRedirectResult } from 'firebase/auth';
+
 import {
+	auth,
 	signInWithGooglePopup,
+	signInWithGoogleRedirect,
 	createUserDocumentFromAuth,
 } from '../../utils/firebase/firebase.utils';
+import { resolvePath } from 'react-router-dom';
 
 const SignIn = () => {
+	useEffect(() => {
+		async function getResult() {
+			const response = await getRedirectResult(auth);
+			if (response) {
+				const userDocRef = await createUserDocumentFromAuth(
+					response.user
+				);
+			}
+		}
+		getResult();
+	}, []);
+
 	const logGoogleUser = async () => {
 		const { user } = await signInWithGooglePopup();
 		createUserDocumentFromAuth(user);
@@ -12,6 +30,9 @@ const SignIn = () => {
 		<div>
 			<div>Sign in page</div>
 			<button onClick={logGoogleUser}>Sign in with Google Popup</button>
+			<button onClick={signInWithGoogleRedirect}>
+				Sign in with Google Redirect
+			</button>
 		</div>
 	);
 };
