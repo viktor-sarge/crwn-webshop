d9398c22ae78a267a9a0c2c8418e04e7f8b9aa69
Viktor Sarge <viktor.sarge@regionhalland.se>
2022-11-29 19:29:31 +0200

    feat(auth): 🛂 Add logout
    Lagt till utloggning

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,13 +1,16 @@
 import { useContext } from 'react';
 import { UserContext } from '../../contexts/user.context';
 import { Outlet, Link } from 'react-router-dom';
-
+import { signOutUser } from '../../utils/firebase/firebase.utils';
 import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
 import './navigation.styles.scss';
 
 const Navigation = () => {
-	const { currentUser } = useContext(UserContext);
-	console.log('in context:', currentUser);
+	const { currentUser, setCurrentUser } = useContext(UserContext);
+	const signOutHandler = async () => {
+		await signOutUser();
+		setCurrentUser = null;
+	};
 	return (
 		<>
 			<div className="navigation">
@@ -18,9 +21,15 @@ const Navigation = () => {
 					<Link className="nav-link" to="/shop">
 						SHOP
 					</Link>
-					<Link className="nav-link" to="/auth">
-						SIGN IN
-					</Link>
+					{currentUser ? (
+						<span className="nav-link" onClick={signOutHandler}>
+							Log out
+						</span>
+					) : (
+						<Link className="nav-link" to="/auth">
+							SIGN IN
+						</Link>
+					)}
 				</div>
 			</div>
 			<Outlet />
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -4,7 +4,8 @@ import {
     signInWithPopup,
     GoogleAuthProvider,
     createUserWithEmailAndPassword, 
-    signInWithEmailAndPassword
+    signInWithEmailAndPassword,
+    signOut,
 } from 'firebase/auth';
 
 import {getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
@@ -68,3 +69,5 @@ export const signInAuthUserWithEmailAndPassword = async (email, password) => {
     if(!email || !password) return;
     return await signInWithEmailAndPassword(auth, email, password)
 };
+
+export const signOutUser = async () => await signOut(auth);
