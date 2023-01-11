949143e7adc5f73869b954f779a6bfa617bc9226
Viktor Sarge <viktor.sarge@regionhalland.se>
2023-01-11 16:58:58 +0200

    style(app): 🔥 Cleanup
    Rensat upp console logs

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -41,7 +41,6 @@ export const addCollectionAndDocuments = async (collectionKey, objectsToAdd) => 
     })
 
     await batch.commit();
-    console.log("Batch done")
 };
 
 export const getCategoriesAndDocuments = async () => {
@@ -60,11 +59,8 @@ export const createUserDocumentFromAuth = async (userAuth, additionalInformation
     if(!userAuth) return;
 
     const userDocRef = doc(db, 'users', userAuth.uid );
-    console.log(userDocRef);
 
     const userSnapshot = await getDoc(userDocRef);
-    console.log(userSnapshot);
-    console.log(userSnapshot.exists());
 
     if(!userSnapshot.exists()) {
         const {displayName, email} = userAuth;
