204b2f7ae224293fb280f10f92c885fd18b8f61c
Viktor Sarge <viktor.sarge@regionhalland.se>
2023-01-12 11:10:37 +0200

    build(app): 🔧 Allow the firebase config to be comitted so Netlify build works
    Släppt in firebase configen i repot så att Netlify kan bygga utan fel

---
+++ src/utils/firebase/firebase-config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBi6_0-flIRK4WoIo2CTB0WtwTXHPSCN9k",
+    authDomain: "crwn-clothing-webshop-db.firebaseapp.com",
+    projectId: "crwn-clothing-webshop-db",
+    storageBucket: "crwn-clothing-webshop-db.appspot.com",
+    messagingSenderId: "1023727101088",
+    appId: "1:1023727101088:web:fab0ec8c09a46f21d0d5c5"
+};
+
+export default firebaseConfig
